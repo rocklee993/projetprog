245878bfa0b383aa5f0484038a8d4cfa5dd519dd
Rocklee <guebghid7@gmail.com>
2023-11-03 12:40:12 +0100

    Updating Character

--- character.cpp
+++ character.cpp
@@ -1,6 +1,6 @@
 #include "character.h"
 
-character::character()
+character::character(int x,int y,int health,int damage,bool player,char symbol):d_posx{x},d_posy{y},d_health{health},d_damage{damage},d_isPlayer{player},d_symbol{symbol}
 {
     //ctor
 }
@@ -8,4 +8,24 @@ character::character()
 character::~character()
 {
     //dtor
+}
+int character::getx()const{
+    return d_posx;
+}
+int character::gety()const{
+    return d_posy;
+}
+int character::getdamage()const{
+    return d_damage;
+}
+int character::gethealth()const{
+    return d_health;
+}
+char character::getSymbol()const{
+    return d_symbol;
+}
+void character::move(int dx, int dy){
+
+    d_posx += dx;
+    d_posy += dy;
 }
--- character.h
+++ character.h
@@ -5,10 +5,20 @@
 class character
 {
     public:
-        character();
+        character(int x,int y,int health,int damage,bool player,char symbol);
+        void move(int dx, int dy);
         virtual ~character();
+        int getx()const;
+        int gety()const;
+        int gethealth()const;
+        int getdamage()const;
+        char getSymbol() const;
 
     protected:
+        int d_health, d_damage;
+        int d_posx,d_posy;
+        bool d_isPlayer;
+        char d_symbol;
 
     private:
 };
