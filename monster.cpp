3199addb9b089dc1febe8bc66ba17bf06e133817
Rocklee <guebghid7@gmail.com>
2023-11-04 12:58:45 +0100

    updating monster class

--- monster.cpp
+++ monster.cpp
@@ -1,11 +1,11 @@
 #include "monster.h"
 
-monster::monster()
+monster::monster(int x,int y):character(x,y,5,10,false,'m')
 {
     //ctor
 }
 
 monster::~monster()
 {
-    //dtor
+
 }
--- monster.h
+++ monster.h
@@ -1,18 +1,25 @@
 #ifndef MONSTER_H
 #define MONSTER_H
-
+#include <memory>
+#include <vector>
 #include "character.h"
 
 
 class monster : public character
 {
     public:
-        monster();
+        monster(int x,int y);
         virtual ~monster();
+        void addmonster(monster& m);
+        std::vector<std::unique_ptr<monster>> monsters;
+
 
     protected:
 
     private:
+        static constexpr bool d_isPlayer = false;
+
+
 };
 
 #endif // MONSTER_H
