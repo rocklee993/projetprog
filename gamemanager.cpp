8eea3d966f996f82f8a4ff83447577349f3fde53
YonbiDev <74678000+YonbiDev@users.noreply.github.com>
2024-01-11 23:10:54 +0100

    Update gamemanager.cpp

--- gamemanager.cpp
+++ gamemanager.cpp
@@ -20,7 +20,7 @@ void gamemanager::gameover() {
 
 d_gameEnded = true;
 
-std::cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl << "GAMEOVER";
+std::cout << "!!!!!GAMEOVER!!!!!" << std::endl;
 }
 
 
@@ -41,31 +41,61 @@ void gamemanager::monsterTurn(){
 
 
  for (auto& m : d_monsters) {
-        cout << "hello";
-
-
     if(m->getSymbol() == 'm') {
     m->chooseDirection(d_ruins.getWallPositions());
     } else {
         m->chooseDirection(d_ruins.getWallPositions(),&d_player);
     }
-    int newX = m->getX();
-    int newY = m->getY();
+
+    if (d_player.getX() == m->getX() && d_player.getY() == m->getY()) {
+                 std::cout << "**** FIGHT (TATAKAY) ****" << std:: endl;
+        m->attack(d_player);
+        if (d_player.getHealth() <= 0) {
+                d_gameEnded = true;
+                return;
+            }
+
+    d_player.attack(*m); // it's working
+
+     if (m->getHealth() <= 0 && m != nullptr ) {
+    auto it = std::remove(d_monsters.begin(), d_monsters.end(), m);
+    d_monsters.erase(it, d_monsters.end());
+     std::cout << "****The player defeated the enemy, good job :)****" << std:: endl;
+
+}
+        }
         }
 
 
+
+         // it's working
+
+
  }
+
 void gamemanager::playerTurn() {
 
 
     d_player.chooseDirection(d_ruins.getWallPositions());
+     system("cls");
     int newX = d_player.getX();
     int newY = d_player.getY();
 
     if (d_ruins.isTreasure(newX,newY))
     {
-        exit(0);
+       // d_player.foundTreasure();
+       d_ruins.setExit();
     }
+
+    if(d_ruins.getExit()) {
+
+        std::vector<int> exitPos = d_ruins.getExitPosition();
+
+    if(newX == exitPos[1] && newY == exitPos[0]) {
+      win();
+    }
+    }
+
 
    for (auto it = d_ruins.getCoinPositions().begin(); it != d_ruins.getCoinPositions().end(); )
 {
@@ -74,7 +104,7 @@ void gamemanager::playerTurn() {
         d_player.addbourse();
         cout << "!!!!!!!!!!" << std::endl;
         Sleep(1000);
-        cout << "You hit the jackpot! You collected a coin." << std::endl;
+        cout << "****You hit the jackpot! You collected a coin.****" << std::endl;
         it = d_ruins.getCoinPositions().erase(it);
     }
     else
@@ -84,66 +114,62 @@ void gamemanager::playerTurn() {
 }
 
 
-<<<<<<< HEAD
       for (auto& m : d_monsters) {
+            if(m != nullptr) {
+
     if (d_player.getX() == m->getX() && d_player.getY() == m->getY()) {
+                std::cout << "**** FIGHT (TATAKAY) ****" << std:: endl;
         d_player.attack(*m); // it's working
 
-    //   d_monsters.erase(
-    //std::remove_if(d_monsters.begin(), d_monsters.end(),
-      //  [](const std::unique_ptr<monster>& m) {
-        //    return m != nullptr && m->getHealth() <= 0;
-       // }),
-   // d_monsters.end());
+     if (m->getHealth() <= 0 && m != nullptr ) {
+    auto it = std::remove(d_monsters.begin(), d_monsters.end(), m);
+    d_monsters.erase(it, d_monsters.end());
+    std::cout << " ****The player defeated the enemy, good job :)****" << std:: endl;
 
-        if (m != nullptr) {
-=======
-      for (auto it = d_monsters.begin(); it != d_monsters.end(); ) {
-    auto& m = *it;
-
-    if (d_player.getX() == m->getX() && d_player.getY() == m->getY()) {
-        d_player.attack(*m);
-
-        if (m->getHealth() > 0) {
->>>>>>> lyes
-            m->attack(d_player);
-            if (d_player.getHealth() <= 0) {
-                gameover();
+} else {
+    m->attack(d_player);
+       if (d_player.getHealth() <= 0) {
+                  d_gameEnded = true;
             }
-<<<<<<< HEAD
-        }
-    }
-
-=======
-        } else {
-            // Monster is dead
-            cout << "Monster defeated!" << std::endl;
-            it = d_monsters.erase(it);  // Remove the monster from the vector
-            continue;  // Skip the rest of the loop
-        }
-    }
-
-    ++it;  // Move to the next monster
->>>>>>> lyes
 }
+            }
 
 
-d_player.showstats(cout);
+    }
 
+}
 }
 void gamemanager::startGame() {
 
 
    while (!d_gameEnded) {
+         d_player.showstats(cout);
     d_ruins.initializeRuins(d_player, d_monsters);
     d_ruins.render();
+        instruction();
     playerTurn();
     monsterTurn();
+
+
+
+// ff
 }
+gameover();
+
+
 }
 void gamemanager::win() {
-
+       d_player.foundTreasure();
+        cout << "BRAVO! You are a hero ! :D";
+        exit(0);
 }
 
-
-
+void gamemanager::instruction() {
+    std::cout << "***********Instructions for Movement :***********" << std::endl;
+    std::cout << "*   (left up)           (up)         (right up)  *" << std::endl;
+    std::cout << "*               7        8        9              *" << std::endl;
+    std::cout << "*      (left)   4        5        6  (right)     *" << std::endl;
+    std::cout << "*               1        2        3              *" << std::endl;
+    std::cout << "* (left down)          (down)        (right down)*" << std::endl;
+    std::cout << "**************************************************" << std::endl;
+}
